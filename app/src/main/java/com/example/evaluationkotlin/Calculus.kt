64ade3f593d5b28075a6835b3249f7d13e876a47
Dragoş <edragos33@gmail.com>
2025-01-20 15:55:58 +0100

    kinda added functions for the calculus

--- app/src/main/java/com/example/evaluationkotlin/Calculus.kt
+++ app/src/main/java/com/example/evaluationkotlin/Calculus.kt
@@ -4,4 +4,20 @@ class Calculus {
     fun add (a: Int, b: Int): Int{
         return a + b
     }
+
+    fun substract(a: Int,b: Int): Int{
+        return a - b
+    }
+
+    fun multiply(a:Int,b: Int):Int{
+        return a * b
+    }
+
+    fun divide(a: Int,b: Int): Int{
+        return a / b
+    }
+
+    fun modulo(a: Int,b: Int):Int{
+        return a % b
+    }
 }
